169c4402a0ffc10d3b5ba4036f51dd49aac950cd
Fernando Serra <fernando.serra.r@gmail.com>
2022-03-19 17:14:34 -0300

    Add TableView

--- IOSAppDevelepment/ViewController.swift
+++ IOSAppDevelepment/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK - referencia a la tabla
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
