98851a7723f5a34804ec6481f252a5166f053f93
Fernando Serra <fernando.serra.r@gmail.com>
2022-03-22 23:56:27 -0300

    Add TableView

--- IOSAppDevelepment/ViewController.swift
+++ IOSAppDevelepment/ViewController.swift
@@ -19,9 +19,33 @@ class ViewController: UIViewController {
         
         showDataTablaView.dataSource=self
         
-        
+        showDataTablaView.register(UITableViewCell.self, forCellReuseIdentifier: "mi-celda")
     }
 
 
+}
+
+
+//MARK - UITableViewDataSource
+extension ViewController: UITableViewDataSource{
+    //1) Numero de filas que tendra nuestra tabla
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+        //return dataSource.count
+    }
+    
+    //2) Metodo para sabes que celdas deben mostrarse
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mi-celda", for: indexPath)
+        
+          cell.textLabel?.text = "Soy la celda #\(indexPath.row)"
+        
+ 
+    
+        
+        return cell
+    }
 }
 
