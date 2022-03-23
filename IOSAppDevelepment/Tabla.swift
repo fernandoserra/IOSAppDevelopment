8fd24b19f24252efde0d408986ea19ef6a2a39b9
Fernando Serra <fernando.serra.r@gmail.com>
2022-03-23 15:33:38 -0300

    Create Tabla.swift

--- IOSAppDevelepment/Tabla.swift
+++ IOSAppDevelepment/Tabla.swift
@@ -4,5 +4,35 @@
 //
 //  Created by Fernando Serra on 23-03-22.
 //
+import UIKit
 
-import Foundation
+class TablaController: UIViewController {
+    
+    // MARK - referencia a la tabla
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "mi-celda")
+    
+    }
+}
+
+
+extension TablaController:UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+           return 50
+    }
+   
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "mi-celda", for: indexPath)
+        cell.textLabel?.text = "Celda #\(indexPath.row)"
+        return cell
+    }
+    
+}
